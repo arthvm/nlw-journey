ef56e3bda9971cd3ee2ebbd2370ff5af3c2008bb
Arthur <90999445+arthvm@users.noreply.github.com>
2024-07-09 19:21:47 -0300

    feat: Implement route to get all links of a given trip

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 app.register(cors, {
@@ -25,6 +26,7 @@ app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server Running!");
