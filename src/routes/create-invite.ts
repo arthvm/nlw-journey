701f87a3167a4b678b16a33dc4b427f1f8cf7f71
Arthur <90999445+arthvm@users.noreply.github.com>
2024-07-12 14:21:44 -0300

    revert: Revert confirmation link to not include tripId

--- src/routes/create-invite.ts
+++ src/routes/create-invite.ts
@@ -46,7 +46,7 @@ export async function createInvite(app: FastifyInstance) {
       const formattedEndDate = dayjs(trip.ends_at).format("LL");
 
       const mail = await getMailClient();
-      const confirmationLink = `${env.WEB_BASE_URL}/trips/${tripId}/confirm/${participant.id}`;
+      const confirmationLink = `${env.WEB_BASE_URL}/participants/${participant.id}/confirm`;
 
       const message = await mail.sendMail({
         from: {
